94d994ddbf893fa697b16887f27449f3c81ef392
manees <singhmanish5208@gmail.com>
2024-08-19 03:49:10 -0500

    added cors

---
+++ server/app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const config = require('./config');
+
+
+// Import routes
+
+const authRoutes = require('./routes/auth');
+
+const app = express();
+
+//Middleware
+
+app.use(cors()); // enables cors globally for all routes
+
